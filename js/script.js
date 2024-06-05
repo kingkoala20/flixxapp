65c5c2af8487bb7ea8c3ab8923e47795fc06ab80
Jhun Percia <jhun.percia@music.yamaha.com>
2024-06-05 15:44:39 +0900

    [add] show details

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ async function displayPopularMovies() {
     div.classList.add('card');
 
     const anchorElm = document.createElement('a');
-    anchorElm.setAttribute('href', `/movie-details.html?id=${movie.id}`)
+    anchorElm.setAttribute('href', `/movie-details.html?id=${movie.id}`);
 
     const posterImg = document.createElement('img');
     posterImg.setAttribute(
@@ -25,7 +25,7 @@ async function displayPopularMovies() {
     posterImg.classList.add('card-img-top');
     posterImg.setAttribute('alt', movie.title);
     anchorElm.appendChild(posterImg);
-    div.appendChild(anchorElm)
+    div.appendChild(anchorElm);
 
     const divBody = document.createElement('div');
     divBody.classList.add('card-body');
@@ -65,6 +65,9 @@ async function displayPopularShows() {
     const div = document.createElement('div');
     div.classList.add('card');
 
+    const anchorElm = document.createElement('a');
+    anchorElm.setAttribute('href', `/tv-details.html?id=${show.id}`);
+
     const posterImg = document.createElement('img');
     posterImg.setAttribute(
       'src',
@@ -74,7 +77,8 @@ async function displayPopularShows() {
     );
     posterImg.classList.add('card-img-top');
     posterImg.setAttribute('alt', show.title);
-    div.appendChild(posterImg);
+    anchorElm.appendChild(posterImg);
+    div.appendChild(anchorElm);
 
     const divBody = document.createElement('div');
     divBody.classList.add('card-body');
@@ -271,6 +275,60 @@ async function dispayMovieDetails() {
   movieDetailsContainer.appendChild(divBottom);
 }
 
+async function displayShowDetails() {
+  const showId = window.location.search.split('=')[1];
+
+  const show = await fetchAPIData(`/tv/${showId}`);
+  // Overlay
+
+  displayBackgroundImage('tv', show.backdrop_path);
+
+  const div = document.createElement('div');
+  div.innerHTML = `<div class="details-top">
+          <div>
+            <img
+              src=${
+                show.poster_path
+                  ? `https://image.tmdb.org/t/p/w500${show.poster_path}`
+                  : 'images/no-image.jpg'
+              }
+              class="card-img-top"
+              alt="${show.name}"
+            />
+          </div>
+          <div>
+            <h2>${show.name}</h2>
+            <p>
+              <i class="fas fa-star text-primary"></i>
+              ${show.vote_average.toFixed(1)} / 10
+            </p>
+            <p class="text-muted">Last Release Date: ${show.last_air_date}</p>
+            <p>
+              ${show.overview}
+            </p>
+            <h5>Genres</h5>
+            <ul class="list-group">
+              ${show.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
+            </ul>
+            <a href="${show.homepage}" target="_blank" class="btn">Visit Show Homepage</a>
+          </div>
+        </div>
+        <div class="details-bottom">
+          <h2>Show Info</h2>
+          <ul>
+            <li><span class="text-secondary">Number Of Episodes:</span> ${show.number_of_episodes}</li>
+            <li>
+              <span class="text-secondary">Last Episode To Air:</span> ${show.last_episode_to_air.name}
+            </li>
+            <li><span class="text-secondary">Status:</span> ${show.status}</li>
+          </ul>
+          <h4>Production Companies</h4>
+          <div class="list-group">${show.production_companies.map((company) => company.name).join(', ')}</div>
+        </div>`;
+
+  document.querySelector('#show-details').appendChild(div);
+}
+
 // Display Backdrop on Details Pages
 function displayBackgroundImage(type, path) {
   const overlayDiv = document.createElement('div');
@@ -320,7 +378,6 @@ function hideSpinner() {
 function highlightActiveLink() {
   const links = document.querySelectorAll('.main-header .container .nav-link');
 
-  let targetLink;
   links.forEach((linkElm) => {
     if (linkElm.getAttribute('href') === global.currentPage) {
       linkElm.classList.add('active');
@@ -340,6 +397,9 @@ function init() {
       break;
     case '/movie-details.html':
       dispayMovieDetails();
+      break;
+    case '/tv-details.html':
+      displayShowDetails();
       break;
     case '/search.html':
       console.log('Search');
